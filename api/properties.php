27bbe31fd890f6bf6a44202ae70e663716fb9318
jfgaviria@gmail.com <juanfergaviria>
2018-05-25 14:40:49 +0000

    File generated by Skaffolder - user: jfgaviria@gmail.com

---
+++ api/properties.php
@@ -0,0 +1,10 @@
+<?php 
+	$db_JaoMuch_db_Url = 'localhost';
+	$db_JaoMuch_db_User = 'root';
+	$db_JaoMuch_db_Pass = '';
+	$db_JaoMuch_db_DbName = 'jaomuch_db';
+	
+	
+	$jwt_secret_key = 'mySecretKey';
+	
+?>
